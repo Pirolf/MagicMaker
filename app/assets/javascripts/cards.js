1ba795a32c686ef03e31954051ab9269b66711bc
Pirolf <yuchengtu.uwm@gmail.com>
2015-09-18 01:23:47 -0700

    use background image, cover,and preload

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,16 +1,27 @@
 $(function() {
-  //pageReady();
+  pageReady();
 });
-//$(document).on('page:load', pageReady);
+$(document).on('page:load', pageReady);
 $(document).on('change', '#card_image_file_field', function(){
   previewImage(this);
 });
+function pageReady(){
+  loadArt();
+}
+function loadArt(){
+  //preload
+  var artImage = new Image();
+  artImage.onload = function(){
+      $('.card_art_container').css('background-image', 'url(' + artUrl + ')'); 
+  }
+  var artUrl = $('.card_art_container').attr('data-art-url');
+  artImage.src = artUrl;
+}
 function previewImage(fileInput){
   if (fileInput && fileInput.files[0]){
     var reader = new FileReader();
     reader.onload = function (e){
-      console.log("loading");
-      $('.card_art').attr('src', e.target.result);
+      $('.card_art_container').css('background-image', 'url(' + e.target.result + ')');
     }
     reader.readAsDataURL(fileInput.files[0]);
   }
