cfd4fcbfbe5e928f76cc730db8f53ea3708f3953
Pirolf <yuchengtu.uwm@gmail.com>
2015-09-30 01:58:24 -0700

    scale * 0.8 when show all

--- app/assets/javascripts/card_all.js
+++ app/assets/javascripts/card_all.js
@@ -4,6 +4,7 @@ function pageReady(){
 	var cardContainers = $('.card_container');
 	cardContainers.each(function(i, e){
 		var imagesUrl = $(this).attr('data-image-url');
+		$(this).css('transform', 'scale(0.8,0.8)');
 		var card  = new CardPresenter($(this));
 		card.loadArt();
 		card.loadCardFrame(imagesUrl);
