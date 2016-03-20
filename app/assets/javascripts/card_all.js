0235f9bedba9c896d2e3ad96269a856721d35ccb
Pirolf <pirolf@toro.local>
2016-03-20 00:36:00 -0700

    use flex instead of bootstra on cards index page

--- app/assets/javascripts/card_all.js
+++ app/assets/javascripts/card_all.js
@@ -4,8 +4,7 @@ $(function(){
 	var cardContainers = $('.card_container');
 	cardContainers.each(function(i, e){
 		var imagesUrl = $(this).attr('data-image-url');
-		$(this).css('transform-origin', 'left top');
-		$(this).css('transform', 'scale(0.7,0.7)');
+		
 		var card  = new CardPresenter($(this));
 		card.loadArt();
 		card.loadCardFrame(imagesUrl);
